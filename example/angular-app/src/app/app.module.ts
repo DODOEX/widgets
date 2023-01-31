dc0e94386e47c9d67e2ac37fe89f2ca6b73681a1
huang.zw <turbohzw@icloud.com>
2023-01-31 16:23:00 +0800

    feat: init angular-app example.

--- example/angular-app/src/app/app.module.ts
+++ example/angular-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WidgetsComponent } from './widgets/widgets.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WidgetsComponent
   ],
   imports: [
     BrowserModule,
---
+++ example/angular-app/src/app/widgets/widgets.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-widgets',
+  templateUrl: './widgets.component.html',
+  styleUrls: ['./widgets.component.scss']
+})
+export class WidgetsComponent {
+
+}
+
