36df365e7c60423eaf2e7e0ef0765185a3d643a6
huang.zw <turbohzw@icloud.com>
2023-03-03 23:58:38 +0800

    fix: restore deleted code.

--- packages/dodoex-widgets/src/components/Swap/index.tsx
+++ packages/dodoex-widgets/src/components/Swap/index.tsx
@@ -384,63 +384,63 @@ export function Swap() {
     const isBasicToken = basicTokenAddress === fromToken?.address;
     const balance = new BigNumber(fromTokenBalance || 0);
 
-    // if (!account) return <ConnectWallet />;
-    // if (isInflight) {
-    //   return (
-    //     <Button fullWidth isLoading disabled>
-    //       <Trans>Transaction Pending</Trans>
-    //     </Button>
-    //   );
-    // }
-    // if (!fromToken || !toToken)
-    //   return (
-    //     <Button fullWidth disabled data-testid={swapAlertSelectTokenBtn}>
-    //       <Trans>Select Tokens</Trans>
-    //     </Button>
-    //   );
-    // if (needApprove)
-    //   return (
-    //     <Button
-    //       fullWidth
-    //       disabled={isApproving}
-    //       onClick={() => submitApprove(fromToken)}
-    //     >
-    //       {isApproving ? <Trans>Approving</Trans> : <Trans>Approve</Trans>}
-    //     </Button>
-    //   );
-    // if (!new BigNumber(isReverseRouting ? toAmt : fromAmt).gt(0))
-    //   return (
-    //     <Button fullWidth disabled data-testid={swapAlertEnterAmountBtn}>
-    //       <Trans>Enter an amount</Trans>
-    //     </Button>
-    //   );
-    // if (resPriceStatus === RoutePriceStatus.Loading)
-    //   return (
-    //     <Button fullWidth disabled data-testid={swapAlertFetchPriceBtn}>
-    //       <Trans>Fetching Price...</Trans>
-    //     </Button>
-    //   );
+    if (!account) return <ConnectWallet />;
+    if (isInflight) {
+      return (
+        <Button fullWidth isLoading disabled>
+          <Trans>Transaction Pending</Trans>
+        </Button>
+      );
+    }
+    if (!fromToken || !toToken)
+      return (
+        <Button fullWidth disabled data-testid={swapAlertSelectTokenBtn}>
+          <Trans>Select Tokens</Trans>
+        </Button>
+      );
+    if (needApprove)
+      return (
+        <Button
+          fullWidth
+          disabled={isApproving}
+          onClick={() => submitApprove(fromToken)}
+        >
+          {isApproving ? <Trans>Approving</Trans> : <Trans>Approve</Trans>}
+        </Button>
+      );
+    if (!new BigNumber(isReverseRouting ? toAmt : fromAmt).gt(0))
+      return (
+        <Button fullWidth disabled data-testid={swapAlertEnterAmountBtn}>
+          <Trans>Enter an amount</Trans>
+        </Button>
+      );
+    if (resPriceStatus === RoutePriceStatus.Loading)
+      return (
+        <Button fullWidth disabled data-testid={swapAlertFetchPriceBtn}>
+          <Trans>Fetching Price...</Trans>
+        </Button>
+      );
 
-    // if (!resAmount)
-    //   return (
-    //     <Button fullWidth disabled>
-    //       <Trans>Quote not available</Trans>
-    //     </Button>
-    //   );
-    // if (
-    //   balance.lt(isReverseRouting ? resAmount : fromAmt) ||
-    //   (isBasicToken && balance.lte(keepChanges))
-    // )
-    //   // balance need to greater than reserved gas!
-    //   return (
-    //     <Button
-    //       fullWidth
-    //       disabled
-    //       data-testid={swapAlertInsufficientBalanceBtn}
-    //     >
-    //       <Trans>Insufficient balance</Trans>
-    //     </Button>
-    //   );
+    if (!resAmount)
+      return (
+        <Button fullWidth disabled>
+          <Trans>Quote not available</Trans>
+        </Button>
+      );
+    if (
+      balance.lt(isReverseRouting ? resAmount : fromAmt) ||
+      (isBasicToken && balance.lte(keepChanges))
+    )
+      // balance need to greater than reserved gas!
+      return (
+        <Button
+          fullWidth
+          disabled
+          data-testid={swapAlertInsufficientBalanceBtn}
+        >
+          <Trans>Insufficient balance</Trans>
+        </Button>
+      );
     return (
       <Button
         fullWidth
