f42555788ea4c9359b37acc19541315b209a1615
junjie <wangjunjie0817@foxmail.com>
2023-06-02 16:38:22 +0800

    feat: route support more chains

--- packages/dodoex-widgets/src/constants/api.ts
+++ packages/dodoex-widgets/src/constants/api.ts
@@ -1,15 +1,17 @@
 import { ChainId } from './chains';
 
-export const RoutePriceAPI = `https://api.dodoex.io/route-service/v2/widget/getdodoroute`;
+// export const RoutePriceAPI = `https://api.dodoex.io/route-service/v2/widget/getdodoroute`;
+export const RoutePriceAPI = `https://api.devus.dregon.cc/route/widget/getdodoroute`;
 export const FiatPriceAPI = `https://api.dodoex.io/frontend-price-api`;
 export const AppUrl = `https://app.dodoex.io`;
 
 export const getCGTokenListAPI = (chainId: ChainId) => {
   const defaultCGAPI = 'https://tokens.coingecko.com/uniswap/all.json';
   const cgAPIMap: any = {
-    [ChainId.ARBITRUM_ONE]: 'https://tokens.coingecko.com/arbitrum-one/all.json',
+    [ChainId.ARBITRUM_ONE]:
+      'https://tokens.coingecko.com/arbitrum-one/all.json',
     [ChainId.AURORA]: 'https://tokens.coingecko.com/aurora/all.json',
     [ChainId.BSC]: 'https://tokens.coingecko.com/binance-smart-chain/all.json',
-  }
-  return cgAPIMap[chainId] || defaultCGAPI
+  };
+  return cgAPIMap[chainId] || defaultCGAPI;
 };
