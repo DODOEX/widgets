ece2126bd84050b9810f51a7920bba8dbc2767b4
JunJie <wangjunjie0817@foxmail.com>
2025-06-30 14:47:47 +0800

    fix: message component render

--- packages/dodoex-widgets/src/components/Message/index.tsx
+++ packages/dodoex-widgets/src/components/Message/index.tsx
@@ -30,7 +30,7 @@ const positioningStyles = {
   unmounted: 'translateX(500px)',
 };
 
-export default function Message() {
+function MessageInner() {
   const theme = useTheme();
   const { notify, close } = useMessageState();
   const [exited, setExited] = React.useState(true);
@@ -111,221 +111,229 @@ export default function Message() {
 
   const mobileSpace = 20;
   return (
-    <>
-      {ReactDOM.createPortal(
-        <Box
-          component={Snackbar}
-          open={open}
-          autoHideDuration={timeout}
-          onClose={(_: any, reason: SnackbarCloseReason) => {
-            if (reason === 'clickaway') return;
-            close();
-          }}
-          exited={exited}
-          sx={{
-            position: 'fixed',
-            top: mobileSpace,
-            left: mobileSpace,
-            right: mobileSpace,
-            zIndex: 9000,
-            [theme.breakpoints.up('tablet')]: {
-              left: 'auto',
-            },
-          }}
-        >
-          <Transition
-            timeout={{ enter: 400, exit: 400 }}
-            in={open}
-            appear
-            unmountOnExit
-            onEnter={handleOnEnter}
-            onExited={handleOnExited}
-            nodeRef={nodeRef}
+    <Box
+      component={Snackbar}
+      open={open}
+      autoHideDuration={timeout}
+      onClose={(_: any, reason: SnackbarCloseReason) => {
+        if (reason === 'clickaway') return;
+        close();
+      }}
+      exited={exited}
+      sx={{
+        position: 'fixed',
+        top: mobileSpace,
+        left: mobileSpace,
+        right: mobileSpace,
+        zIndex: 9000,
+        [theme.breakpoints.up('tablet')]: {
+          left: 'auto',
+        },
+      }}
+    >
+      <Transition
+        timeout={{ enter: 400, exit: 400 }}
+        in={open}
+        appear
+        unmountOnExit
+        onEnter={handleOnEnter}
+        onExited={handleOnExited}
+        nodeRef={nodeRef}
+      >
+        {(status) => (
+          <Box
+            ref={nodeRef}
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              p: 12,
+              borderRadius: 8,
+              backgroundColor: 'background.paper',
+              color: 'text.primary',
+              border: 'solid 1px',
+              borderColor: 'border.main',
+              boxShadow: '0px 6px 12px 0px rgba(0, 0, 0, 0.10)',
+              transform: positioningStyles[status],
+              transition: 'transform 300ms ease',
+              [theme.breakpoints.up('tablet')]: {
+                p: 20,
+                width: 'auto',
+                maxWidth: 328,
+              },
+            }}
           >
-            {(status) => (
+            <Box
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+              }}
+            >
+              {/* icon */}
               <Box
-                ref={nodeRef}
                 sx={{
-                  display: 'flex',
-                  justifyContent: 'space-between',
-                  p: 12,
-                  borderRadius: 8,
-                  backgroundColor: 'background.paper',
-                  color: 'text.primary',
-                  border: 'solid 1px',
-                  borderColor: 'border.main',
-                  boxShadow: '0px 6px 12px 0px rgba(0, 0, 0, 0.10)',
-                  transform: positioningStyles[status],
-                  transition: 'transform 300ms ease',
-                  [theme.breakpoints.up('tablet')]: {
-                    p: 20,
-                    width: 'auto',
-                    maxWidth: 328,
+                  mt: 7,
+                  '& > svg': {
+                    width: 28,
+                    height: 28,
                   },
+                  [theme.breakpoints.up('tablet')]: {
+                    '& > svg': {
+                      width: 36,
+                      height: 36,
+                    },
+                  },
+                }}
+              >
+                {notify ? icon : ''}
+              </Box>
+              {/* main */}
+              <Box
+                sx={{
+                  ml: 20,
                 }}
               >
                 <Box
                   sx={{
-                    display: 'flex',
-                    alignItems: 'center',
+                    typography: 'caption',
+                    wordBreak: 'break-all',
+                    maxHeight: '50vh',
+                    overflowY: 'auto',
                   }}
                 >
-                  {/* icon */}
+                  {notify?.message}
+                </Box>
+                {notify?.link ? (
                   <Box
+                    component="a"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href={notify.link.outerLink}
                     sx={{
-                      mt: 7,
-                      '& > svg': {
-                        width: 28,
-                        height: 28,
-                      },
-                      [theme.breakpoints.up('tablet')]: {
-                        '& > svg': {
-                          width: 36,
-                          height: 36,
-                        },
+                      display: 'inline-flex',
+                      alignItems: 'center',
+                      typography: 'body2',
+                      color: 'text.secondary',
+                      '&:hover': {
+                        color: 'text.primary',
                       },
                     }}
                   >
-                    {notify ? icon : ''}
-                  </Box>
-                  {/* main */}
-                  <Box
-                    sx={{
-                      ml: 20,
-                    }}
-                  >
+                    {notify.link.text}
                     <Box
+                      component="svg"
+                      width="18"
+                      height="19"
+                      viewBox="0 0 18 19"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
                       sx={{
-                        typography: 'caption',
-                        wordBreak: 'break-all',
-                        maxHeight: '50vh',
-                        overflowY: 'auto',
+                        ml: 4,
+                        width: 18,
+                        height: 18,
                       }}
                     >
-                      {notify?.message}
-                    </Box>
-                    {notify?.link ? (
-                      <Box
-                        component="a"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        href={notify.link.outerLink}
-                        sx={{
-                          display: 'inline-flex',
-                          alignItems: 'center',
-                          typography: 'body2',
-                          color: 'text.secondary',
-                          '&:hover': {
-                            color: 'text.primary',
-                          },
-                        }}
-                      >
-                        {notify.link.text}
-                        <Box
-                          component="svg"
-                          width="18"
-                          height="19"
-                          viewBox="0 0 18 19"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                          sx={{
-                            ml: 4,
-                            width: 18,
-                            height: 18,
-                          }}
-                        >
-                          <path
-                            d="M4.22703 13.2122L10.6811 6.75812L4.75206 6.75812L4.75736 5.25729H13.2426V13.7426H11.7471L11.7418 7.81878L5.28769 14.2729L4.22703 13.2122Z"
-                            fill="currentColor"
-                          />
-                        </Box>
-                      </Box>
-                    ) : (
-                      ''
-                    )}
-                    {notify?.content ? (
-                      <Box
-                        sx={{
-                          mt: 4,
-                          typography: 'body2',
-                          color: 'text.secondary',
-                        }}
-                      >
-                        {notify.content}
-                      </Box>
-                    ) : (
-                      ''
-                    )}
-                  </Box>
-                </Box>
-                {/* close */}
-                <Box
-                  sx={{
-                    position: 'relative',
-                    top: 10,
-                    ml: 12,
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    width: 24,
-                    height: 24,
-                    borderRadius: 12,
-                    borderStyle: 'solid',
-                    borderWidth: timeout ? '2.4px' : 0,
-                    borderColor: theme.palette.border.main,
-                    backgroundColor: timeout
-                      ? 'transparent'
-                      : theme.palette.background.paperDarkContrast,
-                    cursor: 'pointer',
-                    boxSizing: 'border-box',
-                    [theme.breakpoints.up('tablet')]: {
-                      ml: 28,
-                    },
-                  }}
-                  onClick={close}
-                >
-                  {timeout ? (
-                    <Countdown
-                      size={24}
-                      strokeWidth="2.4px"
-                      time={`${timeout}ms`}
-                      color={theme.palette.text.disabled}
-                    />
-                  ) : (
-                    ''
-                  )}
-                  <Box
-                    component="svg"
-                    width="16"
-                    height="16"
-                    viewBox="0 0 16 16"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    sx={{
-                      position: 'absolute',
-                      top: '50%',
-                      left: '50%',
-                      transform: 'translate(-50%, -50%)',
-                      width: 16,
-                      height: 16,
-                      border: 'none',
-                      color: theme.palette.text.disabled,
-                    }}
-                  >
-                    <g>
                       <path
-                        d="M12.8897 4.36345L9.25313 8L12.8897 11.6365L11.6775 12.8487L8.04095 9.21218L4.4044 12.8487L3.19221 11.6365L6.82876 8L3.19221 4.36345L4.4044 3.15127L8.04095 6.78782L11.6775 3.15127L12.8897 4.36345Z"
+                        d="M4.22703 13.2122L10.6811 6.75812L4.75206 6.75812L4.75736 5.25729H13.2426V13.7426H11.7471L11.7418 7.81878L5.28769 14.2729L4.22703 13.2122Z"
                         fill="currentColor"
                       />
-                    </g>
+                    </Box>
                   </Box>
-                </Box>
+                ) : (
+                  ''
+                )}
+                {notify?.content ? (
+                  <Box
+                    sx={{
+                      mt: 4,
+                      typography: 'body2',
+                      color: 'text.secondary',
+                    }}
+                  >
+                    {notify.content}
+                  </Box>
+                ) : (
+                  ''
+                )}
               </Box>
-            )}
-          </Transition>
-        </Box>,
-        document.body,
-      )}
-    </>
+            </Box>
+            {/* close */}
+            <Box
+              sx={{
+                position: 'relative',
+                top: 10,
+                ml: 12,
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                width: 24,
+                height: 24,
+                borderRadius: 12,
+                borderStyle: 'solid',
+                borderWidth: timeout ? '2.4px' : 0,
+                borderColor: theme.palette.border.main,
+                backgroundColor: timeout
+                  ? 'transparent'
+                  : theme.palette.background.paperDarkContrast,
+                cursor: 'pointer',
+                boxSizing: 'border-box',
+                [theme.breakpoints.up('tablet')]: {
+                  ml: 28,
+                },
+              }}
+              onClick={close}
+            >
+              {timeout ? (
+                <Countdown
+                  size={24}
+                  strokeWidth="2.4px"
+                  time={`${timeout}ms`}
+                  color={theme.palette.text.disabled}
+                />
+              ) : (
+                ''
+              )}
+              <Box
+                component="svg"
+                width="16"
+                height="16"
+                viewBox="0 0 16 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                sx={{
+                  position: 'absolute',
+                  top: '50%',
+                  left: '50%',
+                  transform: 'translate(-50%, -50%)',
+                  width: 16,
+                  height: 16,
+                  border: 'none',
+                  color: theme.palette.text.disabled,
+                }}
+              >
+                <g>
+                  <path
+                    d="M12.8897 4.36345L9.25313 8L12.8897 11.6365L11.6775 12.8487L8.04095 9.21218L4.4044 12.8487L3.19221 11.6365L6.82876 8L3.19221 4.36345L4.4044 3.15127L8.04095 6.78782L11.6775 3.15127L12.8897 4.36345Z"
+                    fill="currentColor"
+                  />
+                </g>
+              </Box>
+            </Box>
+          </Box>
+        )}
+      </Transition>
+    </Box>
   );
+}
+
+export default function Message() {
+  const [mounted, setMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setMounted(true);
+
+    return () => setMounted(false);
+  }, []);
+
+  if (mounted) return ReactDOM.createPortal(<MessageInner />, document.body);
+  return null;
 }
