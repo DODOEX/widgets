5451fe6c31599a17ceefbd5423b70292d435a637
yrjkqq <yuangu.1994@gmail.com>
2025-10-10 14:04:47 +0800

    refactor: simplify AddLiquidityV3 component by removing unnecessary state management and API calls; clean up unused imports in stories

--- packages/doc/src/stories/widgets/AMMV3Create.stories.tsx
+++ packages/doc/src/stories/widgets/AMMV3Create.stories.tsx
@@ -1,6 +1,4 @@
-import { SwapWidgetApi } from '@dodoex/api';
-import { AddLiquidityV3, SwapWidgetProps, Widget } from '@dodoex/widgets';
-import React from 'react';
+import { AddLiquidityV3 } from '@dodoex/widgets';
 
 export default {
   title: 'Widgets/AddLiquidityV3',
@@ -8,31 +6,16 @@ export default {
 };
 
 export const Primary = (props: any) => {
-  const [config, setConfig] = React.useState<SwapWidgetProps>({});
-  const { projectId, apiKey, ...other } = props;
-  React.useEffect(() => {
-    if (projectId && apiKey) {
-      const dodoService = new SwapWidgetApi();
-      dodoService
-        .getConfigSwapWidgetProps(projectId, apiKey)
-        .then(({ swapWidgetProps }) => {
-          setConfig(swapWidgetProps);
-        });
-    }
-  }, [projectId, apiKey]);
-
   return (
-    <Widget {...config} {...other} apikey={apiKey}>
-      <AddLiquidityV3
-        handleGoBack={() => window.alert('handleGoBack')}
-        handleGoToPoolList={() => window.alert('handleGoToPoolList')}
-        params={{
-          from: '0x8f2a9f23d5d70226491b0c10365de88f64cd4a01',
-          to: '0xd05553bc85fa8c004073d91097b7611cd5e478f5',
-          fee: '100',
-        }}
-      />
-    </Widget>
+    <AddLiquidityV3
+      handleGoBack={() => window.alert('handleGoBack')}
+      handleGoToPoolList={() => window.alert('handleGoToPoolList')}
+      params={{
+        from: '0x8f2a9f23d5d70226491b0c10365de88f64cd4a01',
+        to: '0xd05553bc85fa8c004073d91097b7611cd5e478f5',
+        fee: '100',
+      }}
+    />
   );
 };
 
