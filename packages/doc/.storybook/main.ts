ecb798dc42fe76bb99d86424a6d17c823348bcb9
JunJie <wangjunjie0817@foxmail.com>
2025-10-13 17:11:47 +0800

    chore: update doc

--- packages/doc/.storybook/main.ts
+++ packages/doc/.storybook/main.ts
@@ -17,19 +17,14 @@ const config: StorybookConfig = {
     getAbsolutePath('@storybook/addon-webpack5-compiler-swc'),
     getAbsolutePath('@storybook/addon-onboarding'),
     getAbsolutePath('@storybook/addon-links'),
-    getAbsolutePath('@storybook/addon-essentials'),
     getAbsolutePath('@chromatic-com/storybook'),
-    getAbsolutePath('@storybook/addon-interactions'),
     getAbsolutePath('@storybook/addon-webpack5-compiler-babel'),
+    getAbsolutePath("@storybook/addon-docs")
   ],
 
   framework: {
     name: getAbsolutePath('@storybook/react-webpack5'),
     options: {},
-  },
-
-  docs: {
-    autodocs: false,
   },
 
   webpackFinal: async (config) => {
@@ -125,6 +120,6 @@ const config: StorybookConfig = {
         ],
       },
     });
-  },
+  }
 };
 export default config;
